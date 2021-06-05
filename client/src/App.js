9b50f9778933a573d25c333c78e935f9fa0068d0
Liuming <lfan.canada@gmail.com>
2021-06-05 23:46:48 +0000

    change App.js to functional component, add basic route

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  // Redirect,
+} from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+const App = () => {
+  const [message, setMessage] = useState('Click the button to load data!')
 
-  fetchData = () => {
+  const fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
       console.log(response.data) // The entire response from the Rails API
 
       console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
+      setMessage(
+        response.data.message
+
+      );
     }) 
   }
 
-  render() {
     return (
       <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
+      
+      <Router>
+        <Route path="/quiz" render={() => <h1>"Quiz 1"</h1>}/>
+            <h1>{ message }</h1>
+            <button onClick={fetchData} >
+              Fetch Data
+            </button>        
+      </Router>
+    </div>
     );
-  }
+
 }
 
 export default App;

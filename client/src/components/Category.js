a63f5737734ce2da1ceaf5b7f743dccc9c05d52c
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-08 20:59:38 +0000

    add props to Category component

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Category = () => {
+const Category = (props) => {
   const [test, setTest] = useState([]);
 
   useEffect(() => {

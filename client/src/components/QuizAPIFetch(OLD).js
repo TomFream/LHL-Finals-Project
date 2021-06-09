748df6643d8c87d2f8511edac98aee6d33b16a2d
Liuming <lfan.canada@gmail.com>
2021-06-09 07:37:21 +0000

    rename and add comment - external api fetch from frontend/Keep for reference only

---
+++ client/src/components/QuizAPIFetch(OLD).js
@@ -0,0 +1,34 @@
+import React, { Component, useState, useEffect } from "react";
+import axios from "axios";
+const apiKey = process.env.REACT_APP_API_KEY;
+
+//FOR REFERENCE ONLY -- OUTDATED FRONTEND API QUIZ FETCH FROM QUIZAPI.IO//
+
+const Quiz = () => {
+  const [test, setTest] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&difficulty=Easy&limit=10`
+      )
+      .then((result) => {
+        console.log("Quiz API", result.data);
+        const testNew = result.data;
+        setTest(() => ({
+          testNew,
+        }));
+      });
+  }, []);
+
+  console.log("test", test);
+  console.log("type", typeof test);
+
+  return (
+    <>
+      <h1>{JSON.stringify(test)}</h1>
+    </>
+  );
+};
+
+export default Quiz;

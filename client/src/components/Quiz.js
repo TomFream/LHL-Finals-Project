64d11ee6a4c16923df9b1b6261a75907000a4e6d
Liuming <lfan.canada@gmail.com>
2021-06-09 07:26:19 +0000

    comment out old quiz list table JSX, style, materials-UI imports

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,33 +1,98 @@
-import React, { Component, useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import QuizTable from "./QuizTable";
+// import { makeStyles } from "@material-ui/core/styles";
+// import List from "@material-ui/core/List";
+// import ListItem from "@material-ui/core/ListItem";
+// import ListItemIcon from "@material-ui/core/ListItemIcon";
+// import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+// import ListItemText from "@material-ui/core/ListItemText";
+// import Checkbox from "@material-ui/core/Checkbox";
+// import Card from "@material-ui/core/Card";
+// import CardActions from "@material-ui/core/CardActions";
+// import CardContent from "@material-ui/core/CardContent";
+// import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
 
-const apiKey = process.env.REACT_APP_API_KEY;
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: "flex",
+//     flexDirection: "column",
+//     align: "center",
 
-const Quiz = () => {
-  const [test, setTest] = useState([]);
+//     width: "100%",
+//     maxWidth: 800,
+//     backgroundColor: theme.palette.background.paper,
+//   },
 
-  useEffect(() => {
-    axios
-      .get(
-        `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&difficulty=Easy&limit=10`
-      )
-      .then((result) => {
-        console.log("Quiz API", result.data);
-        const testNew = result.data;
-        setTest(() => ({
-          testNew,
-        }));
-      });
-  }, []);
+//   pos: {
+//     marginBottom: 50,
+//   },
+// }));
 
-  console.log("test", test);
-  console.log("type", typeof test);
+export default function Quiz() {
+  // const classes = useStyles();
+
+  // const [checked, setChecked] = React.useState([0]);
+
+  // const handleToggle = (value) => () => {
+  //   const currentIndex = checked.indexOf(value);
+  //   const newChecked = [...checked];
+
+  //   if (currentIndex === -1) {
+  //     newChecked.push(value);
+  //   } else {
+  //     newChecked.splice(currentIndex, 1);
+  //   }
+
+  //   setChecked(newChecked);
+  // };
 
   return (
     <>
-      <h1>{JSON.stringify(test)}</h1>
-    </>
+    <QuizTable />
+    </> 
   );
-};
+}
 
-export default Quiz;
+      {/* <Card className={classes.root} style={{ backgroundColor: "orange" }}>
+        <CardContent>
+          <Typography align="left" style={{ FontFamily: "Roboto" }}>
+            Question: Inside which HTML element do we put the JavaScript?
+          </Typography> */}
+
+          {/* <List className={classes.root}> */}
+          {/* {[0, 1, 2, 3].map((value) => {
+            const labelId = `checkbox-list-label-${value}`;
+
+            return (
+              <ListItem
+                key={value}
+                role={undefined}
+                dense
+                button
+                onClick={handleToggle(value)}
+              >
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ "aria-labelledby": labelId }}
+                  />
+                </ListItemIcon>
+                <ListItemText
+                  id={labelId}
+                  primary={`Answer_Option ${value + 1}`}
+                />
+
+              </ListItem>
+            );
+          })} */}
+          {/* </List> */}
+      {/* <Button align="left" variant="contained" color="primary">
+        Submit
+      </Button>
+        </CardContent> */}
+      {/* </Card> */}
+ 

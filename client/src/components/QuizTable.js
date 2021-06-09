04183074262976bef9189907444f3fc55a0db62b
Liuming <lfan.canada@gmail.com>
2021-06-09 18:39:19 +0000

    commented out shuffle feature, add questions from db,render questions dynamically

--- client/src/components/QuizTable.js
+++ client/src/components/QuizTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -14,12 +14,18 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import IconButton from "@material-ui/core/IconButton";
 // import ShuffleOnOutlinedIcon from '@material-ui/icons/ShuffleOnOutlined';
+import Switch from "@material-ui/core/Switch";
+import MobileStepper from "@material-ui/core/MobileStepper";
+import Paper from "@material-ui/core/Paper";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: "lavender",
     width: "100%",
     maxWidth: 800,
+    flexGrow: 10,
   },
   formControl: {
     margin: theme.spacing(3),
@@ -39,9 +45,19 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ErrorRadios() {
   const classes = useStyles();
+  const theme = useTheme();
   const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(false);
   const [helperText, setHelperText] = React.useState("");
+  // const [shuffle, setShuffle] = React.useState({
+  //   checkedA: true,
+  //   checkedB: true,
+  // });
+
+  // const handleShuffleChange = (event) => {
+  //   setShuffle({ ...shuffle, [event.target.name]: event.target.checked });
+  //   // random(answersArr);
+  // };
 
   const handleRadioChange = (event) => {
     setValue(event.target.value);
@@ -70,17 +86,76 @@ export default function ErrorRadios() {
     }
   };
 
-  const answers = [
+  const questions = [
+    {
+      id: 1,
+      question: "Inside which HTML element do we put the JavaScript?",
+      category_id: 1,
+      created_at: "2021-06-08T02:35:20.692Z",
+      updated_at: "2021-06-08T02:35:20.692Z",
+    },
+    {
+      id: 2,
+      question: "What is the correct HTML for making a hyperlink?",
+      category_id: 2,
+      created_at: "2021-06-08T02:35:20.701Z",
+      updated_at: "2021-06-08T02:35:20.701Z",
+    },
+    {
+      id: 3,
+      question: "What is the correct HTML for making a text area?",
+      category_id: 2,
+      created_at: "2021-06-08T02:35:20.748Z",
+      updated_at: "2021-06-08T02:35:20.748Z",
+    },
+  ];
+
+  const answer_options = [
     "Answer_Option1",
     "Answer_Option2, long long answer long.... testing",
     "Answer_Option3",
     "Answer_Option4",
   ];
 
+  const answersArr = [
+    "Answer_Option1",
+    "Answer_Option2, long long answer long.... testing",
+    "Answer_Option3",
+    "Answer_Option4",
+  ];
+
+  // const answersRandom = answersArr.sort(() => Math.random() - 0.5);
+
+  // let answers;
+  // if (shuffle.checkedA) {
+  //   answers = answersRandom
+  // } else {
+  //   answers = answersArr
+  // }
+  // const shuffleToggle = (answersRandom, answersArr) => {
+  //   if (shuffle.checkedA) {
+  //     return answersRandom;
+  //   } else {
+  //     return answersArr;
+  //   }
+  // }
+
+  const [activeQuestion, setActiveQuestion] = React.useState(0);
+  const numQuestions = questions.length;
+
+  const handleNext = () => {
+    setActiveQuestion((preActiveQuestion) => preActiveQuestion + 1);
+  };
+
+  const handleBack = () => {
+    setActiveQuestion((preActiveQuestion) => preActiveQuestion - 1);
+  };
+
   return (
     <Card className={classes.root}>
       <CardContent>
-        <form className={classes.form} onSubmit={handleSubmit}>
+        <form className={classes.form} onSubmit={handleSubmit}>     {/*Show Error Msgs*/}
+          {" "}
           <FormControl
             component="fieldset"
             error={error}
@@ -88,7 +163,7 @@ export default function ErrorRadios() {
           >
             <FormLabel className={classes.label}>
               <Typography align="left" style={{ fontSize: "20px" }}>
-                Pop quiz: Material-UI is...
+                {questions[activeQuestion].question}
               </Typography>
             </FormLabel>
             <RadioGroup
@@ -97,7 +172,7 @@ export default function ErrorRadios() {
               value={value}
               onChange={handleRadioChange}
             >
-              {answers.map((answer) => (
+              {answersArr.map((answer) => (
                 <FormControlLabel
                   className={classes.label}
                   value={`${answer}`}
@@ -105,6 +180,14 @@ export default function ErrorRadios() {
                   label={`${answer}`}
                 />
               ))}
+              {/* {shuffle.checkedA === false && answersArr.map((answer) => (
+                <FormControlLabel
+                  className={classes.label}
+                  value={`${answer}`}
+                  control={<Radio />}
+                  label={`${answer}`}
+                />
+              ))} */}
             </RadioGroup>
             <FormHelperText>{helperText}</FormHelperText>
             <Button
@@ -115,11 +198,17 @@ export default function ErrorRadios() {
             >
               Check Answer
             </Button>
-            <IconButton
+            {/* <Switch
+        checked={shuffle.checkedA}
+        onChange={handleShuffleChange}
+        name="checkedA"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />{`${shuffle.checkedA}`} */}
+            {/* <IconButton
               color="primary"
               aria-label="upload picture"
               component="span"
-            ></IconButton>
+            ></IconButton> */}
           </FormControl>
         </form>
       </CardContent>

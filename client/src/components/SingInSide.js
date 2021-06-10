9c99804c489994d714c3b60997dacf8f98870e47
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-10 21:05:43 +0000

    add dependencies to SignInSide component

--- client/src/components/SingInSide.js
+++ client/src/components/SingInSide.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';

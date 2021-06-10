c1368474b694e5764d038ffa95e0fe8092537f66
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-10 18:14:38 +0000

    add props.name to InputLabel and Input components inside UserInput component

--- client/src/components/UserInput.js
+++ client/src/components/UserInput.js
@@ -1,11 +1,17 @@
-import react, { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Input, InputLabel } from 'material-ui/core';
 
 const UserInput = (props) => {
   return (
-    <Fragment>
-      <InputLabel />
-      <Input />
-    <Fragment />
-  );
+    <Fragment> 
+      <InputLabel
+        name={props.name}
+      />
+      <Input
+        name={props.name}
+      />
+    </ Fragment>
+  )
 }
+
+export default UserInput;

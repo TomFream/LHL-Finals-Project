5a959f2dc234405ddec187797458236bd8a8aafe
Liuming <lfan.canada@gmail.com>
2021-06-10 22:56:54 +0000

    add note

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -2,6 +2,9 @@ import React from 'react';
 import QuizTable from "./QuizTable";
 import useQuizData from "./hooks/useQuizData";
 
+
+//Note: QuizTable --> Quiz --> App.js
+
 export default function Quiz() {
  const {questions, answers } = useQuizData();
 

b22f09b7a58d791ea1668dff2d0c7342bcd28d43
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-10 05:20:45 +0000

    add create method in api/sessions_controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,4 +1,15 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by(name: params[:name])
+    #authenticate user credentials
+    if !!@user && @user.authenticate(params[:password])
+      #set session and redirect on success
+      session[:user_id] = @user.id
+      redirect_to user_path
+    else
+      #error message on fail
+      message = "Something went wrong! Make sure your name and password are correct."
+      redirect_to login_path. notice: message
+    end
   end
 end

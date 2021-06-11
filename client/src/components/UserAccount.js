79ae4948f1fdae4f600693efc40a6b89f4aa2ef2
Liuming <lfan.canada@gmail.com>
2021-06-11 21:15:34 +0000

    add playlist component to user account, style

--- client/src/components/UserAccount.js
+++ client/src/components/UserAccount.js
@@ -5,38 +5,49 @@ import clsx from "clsx";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { useStyles } from "./Component_Style/NavBar";
+import Container from '@material-ui/core/Container';
 import Score from "./Score";
+import Playlist from "./Playlist";
+import { SchoolRounded } from "@material-ui/icons";
 // import { mainListItems, secondaryListItems } from "./UserAccountMenuBarList";  //TODO
 
-//Note: QuizTable + Score --> Quiz --> UserAccount --> NavBar --> App.js
+//Note: QuizTable + Score + Playlist --> UserAccount --> NavBar --> App.js
 
 export default function UserAccount() {
   const { questions, answers } = useQuizData(); ///
-
   const classes = useStyles();
-
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-  return (
-    <div className={classes.root}>
+  return (    
+    <Container maxWidth="lg" className={classes.container}>
       <Grid container spacing={3}>
+        {/* QuizTable */}
         <Grid item xs={12} md={8} lg={9}>
           {/* <Paper className={fixedHeightPaper}> */}
           {questions && answers && (
             <QuizTable questions={questions} answers={answers} />
-          )}
+            )}
           {/* </Paper> */}
         </Grid>
-        {/*Score*/}
+        {/* Score */}
         <Grid item xs={12} md={4} lg={3}>
           <Paper className={fixedHeightPaper}>
             <Score />
           </Paper>
         </Grid>
+        {/* Playlist */}
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Playlist />
+          </Paper>
+        </Grid>
       </Grid>
-    </div>
+    </Container>
   );
 }
+
+
+
 
 // import React from "react";
 // import QuizTable from "./QuizTable";
@@ -141,24 +152,24 @@ export default function UserAccount() {
 //         <div className={classes.appBarSpacer} />
 //         <Container maxWidth="lg" className={classes.container}>
 
-//         <Grid container spacing={3}>
-//             <Grid item xs={12} md={8} lg={9}>
-//               {/* <Paper className={fixedHeightPaper}> */}
-//                 {questions && answers && (
-//               <QuizTable questions={questions} answers={answers} />
-//             )}
-//               {/* </Paper> */}
-//             </Grid>
-//             {/*Score*/}
-//             <Grid item xs={12} md={4} lg={3}>
-//               <Paper className={fixedHeightPaper}><Score /></Paper>
-//             </Grid>
+        // <Grid container spacing={3}>
+        //     <Grid item xs={12} md={8} lg={9}>
+        //       {/* <Paper className={fixedHeightPaper}> */}
+        //         {questions && answers && (
+        //       <QuizTable questions={questions} answers={answers} />
+        //     )}
+        //       {/* </Paper> */}
+        //     </Grid>
+        //     {/*Score*/}
+        //     <Grid item xs={12} md={4} lg={3}>
+        //       <Paper className={fixedHeightPaper}><Score /></Paper>
+        //     </Grid>
 
-//             {/* Extra Component TBD */}
-//           <Grid item xs={12}>
-//           </Grid>
-//             {/* Extra Component TBD */}
-//           </Grid>
+        //     {/* Extra Component TBD */}
+        //   <Grid item xs={12}>
+        //   </Grid>
+        //     {/* Extra Component TBD */}
+        //   </Grid>
 
 //           <Box pt={4}>{/* QuizApp@2021 */}</Box>
 //         </Container>

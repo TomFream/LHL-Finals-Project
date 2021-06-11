62c894382f44825a84ea4c87f2cd0c974fde43fc
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-11 00:06:58 +0000

    changed Login component from class based to functional

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,55 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { 
-  //     username: '',
-  //     email: '',
-  //     password: '',
-  //     errors: ''
-  //    };
-  // }
-[ name, setName ] = setState('');
-[ password, setPassword ] = setState('');
-[ errors, setErrors ] = setState('');
 
-handleChange = (event) => {
+const [ name, setName ] = useState('');
+const [ password, setPassword ] = useState('');
+const [ errors, setErrors ] = useState('');
+
+const handleChange = (event) => {
     const {name, value} = event.target
     setName(value)
   };
-handleSubmit = (event) => {
+const handleSubmit = (event) => {
     event.preventDefault()
   };
-render() {
-    const {username, email, password} = this.statereturn (
+
+return (
       <div>
         <h1>Log In</h1>        
-<form onSubmit={this.handleSubmit}>
-          <input
-            placeholder="username"
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="email"
-            type="text"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />         
-<button placeholder="submit" type="submit">
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="name"
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleChange}
+            />
+            <input
+              placeholder="password"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />         
+          <button placeholder="submit" type="submit">
             Log In
           </button>          
           <div>
@@ -59,6 +44,5 @@ render() {
          </form>
       </div>
     );
-  }
 }
 export default Login;

6c31ae7d7d4103229e70e225971e7893a63045d2
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-13 07:29:08 +0000

    add UserQuizStatsGeneral component, install react-google-charts

---
+++ client/src/components/UserQuizStatsGeneral.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Chart } from 'react-google-charts';
+import { Container } from '@material-ui/core';
+
+const UserQuizStatsGeneral = () => {
+  return (
+    <Container>
+      <Chart
+      
+      />
+    </Container>
+  );
+}
+
+export default UserQuizStatsGeneral

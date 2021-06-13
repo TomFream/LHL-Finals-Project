5e022f070f35df11c917858e537053f3edb14df7
Liuming <lfan.canada@gmail.com>
2021-06-13 18:17:03 +0000

    add draggable notes

--- client/src/components/UserAccount.js
+++ client/src/components/UserAccount.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import QuizTable from "./QuizTable";
 import useQuizData from "./hooks/useQuizData";
 import clsx from "clsx";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { useStyles } from "./Component_Style/NavBar";
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 import Score from "./Score";
 import Playlist from "./Playlist";
-import { SchoolRounded } from "@material-ui/icons";
+import Create from "./Create";
+import Notes from "./Notes";
 // import { mainListItems, secondaryListItems } from "./UserAccountMenuBarList";  //TODO
 
 //Note: QuizTable + Score + Playlist --> UserAccount --> NavBar --> App.js
@@ -18,7 +19,12 @@ export default function UserAccount() {
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-  return (    
+  const [notes, setNotes] = useState([]);
+
+  const notesSubmit = (input) => setNotes([...notes, input]);
+  console.log('notes', notes)
+
+  return (
     <Container maxWidth="lg" className={classes.container}>
       <Grid container spacing={3}>
         {/* QuizTable */}
@@ -26,7 +32,7 @@ export default function UserAccount() {
           {/* <Paper className={fixedHeightPaper}> */}
           {questions && answers && (
             <QuizTable questions={questions} answers={answers} />
-            )}
+          )}
           {/* </Paper> */}
         </Grid>
         {/* Score */}
@@ -39,6 +45,12 @@ export default function UserAccount() {
         <Grid item xs={12}>
           <Paper className={classes.paper}>
             <Playlist />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Notes notes={notes} />
+            <Create setNotes={notesSubmit} />
           </Paper>
         </Grid>
       </Grid>
